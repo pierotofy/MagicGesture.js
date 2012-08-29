ea7110305ddff37b88aa4416b5a70840cf48976f
Piero Toffanin <pierotofy@gmail.com>
2012-08-29 13:21:15 -0500

    Updated version

--- magicgesture.js
+++ magicgesture.js
@@ -21,28 +21,29 @@ OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE. */
 
 var MagicGesture = {
-	_pointToObjectHandler : null,
-	_idToObjectHandler: null,
-	_getIdHandler: null,
-	_setIdHandler: null,
+	_pointToObject : null,
+	_idToObject: null,
+	_getId: null,
+	_setId: null,
 	_touchGroups : {},
-	_touchStartedOn : {}, // elements where a touchstart happened (element => bool)
+	_previousTouchesIn : {},
+	
 
 	/** Initializes magic gesture
-	 * @param pointToObjectHandler (optional) specify a function that finds 
+	 * @param pointToObject (optional) specify a function that finds 
 	 * 		an object at certain screen coordinates. By default the document.elementFromPoint is used.
 	 *		It's useful to specify a custom handler for example when the application does the drawing on a canvas.
-	 * @param idToObjectHandler (optional) specify a function that finds
+	 * @param idToObject (optional) specify a function that finds
 	 *		an object based on its ID. By default document.getElementById is used.
-	 * @param getIdHandler (optional) specify a function that given an element
+	 * @param getId (optional) specify a function that given an element
 	 		returns its id. By default element.id is used. 
-	 * @param setIdHandler (optional) specify a function that given an element
+	 * @param setId (optional) specify a function that given an element
 	 		and an ID, associates the element with a specific id. By default element.id is used.*/
-	init : function(pointToObjectHandler, idToObjectHandler, getIdHandler, setIdHandler){
-		this._pointToObjectHandler = pointToObjectHandler == undefined ? this._defaultPointToObjectHandler : pointToObjectHandler;
-		this._idToObjectHandler = idToObjectHandler == undefined ? this._defaultIdToObjectHandler : idToObjectHandler;
-		this._getIdHandler = getIdHandler == undefined ? this._defaultGetIdHandler : getIdHandler;
-		this._setIdHandler = setIdHandler == undefined ? this._defaultSetIdHandler : setIdHandler;
+	init : function(pointToObject, idToObject, getId, setId){
+		this._pointToObject = pointToObject == undefined ? this._defaultPointToObject : pointToObject;
+		this._idToObject = idToObject == undefined ? this._defaultIdToObject : idToObject;
+		this._getId = getId == undefined ? this._defaultGetId : getId;
+		this._setId = setId == undefined ? this._defaultSetId : setId;
 
 		document.addEventListener('touchstart', this._onTouchStart, false);
 		document.addEventListener('touchmove', this._onTouchMove, false);
@@ -50,10 +51,14 @@ var MagicGesture = {
 	},
 
 	_onTouchStart : function(event) {    
+		if (event.magic) return false; // It's already magic! Don't process again
+
 		MagicGesture._onNumberOfTouchesChanged(event, "ontouchstart");
 	},
 
 	_onTouchMove : function(event) {    
+		if (event.magic) return false; // It's already magic! Don't process again
+
 	    for (var i = 0; i < event.touches.length; i++) {
 	    	// Update touches
 		    for (var id in MagicGesture._touchGroups){
@@ -71,68 +76,107 @@ var MagicGesture = {
 	    for (var id in MagicGesture._touchGroups){
 	    	var group = MagicGesture._touchGroups[id];
 	    	if (group.touches.length == 2){
-	    		var dx = group.touches[0].pageX - group.touches[1].pageX;
-	    		var dy = group.touches[0].pageY - group.touches[1].pageY;
-	    		var newDistance = Math.sqrt(dx * dx + dy * dy); 
-	    		var newAngle = MagicGesture._angleBetweenVectors(group.initialVector, [dx, dy]);
-
-	    		MagicGesture._RaiseEvent("gesturechange", MagicGesture._idToObjectHandler(id), newAngle, newDistance / group.initialDistance);
+	    		if (group.initialDistance != 0){
+		    		var dx = group.touches[0].pageX - group.touches[1].pageX;
+		    		var dy = group.touches[0].pageY - group.touches[1].pageY;
+		    		var newDistance = Math.sqrt(dx * dx + dy * dy); 
+		    		var newAngle = MagicGesture._angleBetweenVectors(group.initialVector, [dx, dy]);
+	    		
+	    			MagicGesture._RaiseGestureEvent("gesturechange", MagicGesture._idToObject(id), newAngle, newDistance / group.initialDistance);
+	    		}
+	    	}else if (group.touches.length == 1){
+	    		MagicGesture._RaiseTouchEvent("touchmove", MagicGesture._idToObject(id), group.touches);
 	    	}
 	    }
-
-
 	},
 
-	_onTouchEnd : function(event) {    
+	_onTouchEnd : function(event) {  
+		if (event.magic) return false; // It's already magic! Don't process again 
+
 		MagicGesture._onNumberOfTouchesChanged(event, "ontouchend"); 
 	},
 
 	/** Called by both onTouchStart and onTouchEnd */
 	_onNumberOfTouchesChanged : function(event, caller){
-	    MagicGesture._touchGroups = {};
+		for (var id in MagicGesture._touchGroups){
+			MagicGesture._touchGroups[id].touches = [];
+		}
 
-	    for (var i = 0; i < event.touches.length; i++) {
-	    	var element = MagicGesture._pointToObjectHandler(event.touches[i].pageX, event.touches[i].pageY);
+		var touches = event.touches;
+		if (touches.length == 0 && event.changedTouches.length > 0){
+			touches = event.changedTouches;
+		}
+
+	    for (var i = 0; i < touches.length; i++) {
+	    	var element = MagicGesture._pointToObject(touches[i].pageX, touches[i].pageY);
 	    	if (!element) continue;
 
 	    	// Not all elements have an ID, if that's the case we'll create one
-			var elementId = MagicGesture._getIdHandler(element);
+			var elementId = MagicGesture._getId(element);
 			if(!elementId) {
-			  MagicGesture._setIdHandler(element, PseudoGuid.GetNew());
+			  MagicGesture._setId(element, PseudoGuid.GetNew());
 			}
 
 	    	if (MagicGesture._touchGroups[elementId] == undefined) MagicGesture._touchGroups[elementId] = {touches:[], initialDistance:0, initialVector:[]};
 
-	    	MagicGesture._touchGroups[elementId].touches.push(event.touches[i]);
+	    	MagicGesture._touchGroups[elementId].touches.push(touches[i]);
 	    }
 
 	    for (var id in MagicGesture._touchGroups){
-	    	var group = MagicGesture._touchGroups[id];
+	    	if (MagicGesture._previousTouchesIn[id] == undefined) MagicGesture._previousTouchesIn[id] = 0;
 
-	    	if (group.touches.length == 2 && !MagicGesture._touchStartedOn[id]){
+	    	var group = MagicGesture._touchGroups[id];
+	    	var numberOfTouchesChanged = MagicGesture._previousTouchesIn[id] != group.touches.length;
+
+	    	if (numberOfTouchesChanged && group.touches.length == 2){
 	    		var dx = group.touches[0].pageX - group.touches[1].pageX;
 	    		var dy = group.touches[0].pageY - group.touches[1].pageY;
 	    		group.initialDistance = Math.sqrt(dx * dx + dy * dy);
 	    		group.initialVector = [dx, dy];
 
-	    		MagicGesture._RaiseEvent("gesturestart", MagicGesture._idToObjectHandler(id), 0, 0);
-	    		MagicGesture._touchStartedOn[id] = true;
-	    	}else if (group.touches.length != 2 && MagicGesture._touchStartedOn[id]){
-	    		MagicGesture._RaiseEvent("gestureend", MagicGesture._idToObjectHandler(id), 0, 0);
-	    		MagicGesture._touchStartedOn[id] = false;
+	    		MagicGesture._RaiseGestureEvent("gesturestart", MagicGesture._idToObject(id), 0, 0);
+	    	}else if (numberOfTouchesChanged && group.touches.length != 2){
+	    		MagicGesture._RaiseGestureEvent("gestureend", MagicGesture._idToObject(id), 0, 0);
 	    	}
+
+
+	    	if (caller == "ontouchstart" && group.touches.length == 1){
+	    		MagicGesture._RaiseTouchEvent("touchstart", MagicGesture._idToObject(id), group.touches);
+	    	}else if (caller == "ontouchend" && group.touches.length == 1){
+	    		MagicGesture._RaiseTouchEvent("touchend", MagicGesture._idToObject(id), group.touches);
+	    	}
+
+	    	MagicGesture._previousTouchesIn[id] = group.touches.length;
 	    }
 	},
 
-	_RaiseEvent : function(name, element, rotation, scale){
+	_RaiseGestureEvent : function(name, element, rotation, scale){
 		var evt = document.createEvent("Event");
 		evt.initEvent(name, true, true);
 		evt.rotation = rotation;
 		evt.scale = scale;
+		evt.magic = true;
+		evt.magicTarget = element;
 
-		if (element.dispatchEvent) element.dispatchEvent(evt);
-		else{
-			evt._target = element;
+		if (element.dispatchEvent){
+		 	element.dispatchEvent(evt);
+		}else{	
+			document.dispatchEvent(evt);
+		}
+	},
+
+	_RaiseTouchEvent : function(name, element, touches){
+		var evt = document.createEvent("Event");
+		evt.initEvent(name, true, true);
+		evt.magicTarget = element;
+		evt.magic = true; // Avoid looping
+		evt.touches = touches;
+		evt.touch = touches[0];
+		evt.changedTouches = touches;
+
+		if (element.dispatchEvent){
+		 	element.dispatchEvent(evt);
+		}else{	
 			document.dispatchEvent(evt);
 		}
 	},
@@ -180,19 +224,19 @@ var MagicGesture = {
 	    return vector_length;
 	},
 
-	_defaultPointToObjectHandler : function(pageX, pageY){
+	_defaultPointToObject : function(pageX, pageY){
 		return document.elementFromPoint(pageX, pageY);
 	},
 
-	_defaultIdToObjectHandler : function(id){
+	_defaultIdToObject : function(id){
 		return document.getElementById(id);
 	},
 
-	_defaultGetIdHandler : function(element){
+	_defaultGetId : function(element){
 		return element.id;
 	},
 
-	_defaultSetIdHandler : function(element, id){
+	_defaultSetId : function(element, id){
 		element.id = id;
 	}
 };
